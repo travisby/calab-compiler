c25ea3f2899ef769b3d9c23773d696e24c2ad13d
Travis Beatty <travisby@gmail.com>
2014-03-02 15:06:26 -0500

    Removed type safety for great good
    In parse we had previously used a series of different data types to
    represent the program at hand.  This created a great tree for us on its
    own while parsing!
    
    Despite that, we cannot create a function to traverse that tree, because
    they were not all of the same type.
    
    The implementation is now different; everything is a constructor for the
    "cst" type.  This allows us to have traversal functions that rely on the
    cst type, which is great.  However, we lost our type checking in the
    constructors.  Now, Block (Close_Brace, Open_Brace, Plus) will
    technically work.  We must take greater caution in the programs
    generating the cst, to ensure the correct relations are kept.
    
    Luckily - because the program was written and worked already with
    everything as a separate type - we are already in that state, providing
    we never create new elements for that cst

--- parse.ml
+++ parse.ml
@@ -2,58 +2,49 @@ open Lex;;
 
 exception Expected_Something_Else of string * Lex.token
 
-type program = Program of block * dollar_sign
-and block = Block of open_brace * statement_list * close_brace
-and statement_list =
+type cst =
+    | Program of cst * cst
+    | Block of cst * cst * cst
     | Emtpy_Statement_List
-    | Statement_List of statement * statement_list
-and statement =
-    | Statement_Print_Statement of print_statement
-    | Statement_Assignment_Statement of assignment_statement
-    | Statement_Var_Decl of var_decl
-    | Statement_While_Statement of while_statement
-    | Statement_If_Statement of if_statement
-    | Statement_Block of block
-and print_statement = Print_Statement of open_paren * expr * close_paren
-and assignment_statement = Assignment_Statement of id * equals * expr
-and var_decl = Var_Decl of _type * id
-and while_statement = While_Statement of boolean_expr * block
-and if_statement = If_Statement of boolean_expr * block
-and expr =
-    | Expr_Int_Expr of int_expr
-    | Expr_String_Expr of string_expr
-    | Expr_Id_Expr of id
-and int_expr = Int_Expr of digit * intop * digit
-and string_expr = String_Expr of quote * char_list * quote
-and boolean_expr = Boolean_Expr of open_paren * expr * boolop * expr * close_paren
-and id = Id of _char
-and char_list =
+    | Statement_List of cst * cst
+    | Statement_Print_Statement of cst
+    | Statement_Assignment_Statement of cst
+    | Statement_Var_Decl of cst
+    | Statement_While_Statement of cst
+    | Statement_If_Statement of cst
+    | Statement_Block of cst
+    | Print_Statement of cst * cst * cst
+    | Assignment_Statement of cst * cst * cst
+    | Var_Decl of cst * cst
+    | While_Statement of cst * cst
+    | If_Statement of cst * cst
+    | Expr_Int_Expr of cst
+    | Expr_String_Expr of cst
+    | Expr_Id_Expr of cst
+    | Int_Expr of cst * cst * cst
+    | String_Expr of cst * cst * cst
+    | Boolean_Expr of cst * cst * cst * cst * cst
+    | Id of cst
     | Char_List of string
     | Empty_Char_List
-and _type =
     | Int | String | Boolean
-and _char =
     | Char of char
-and space =
     | Space
-and digit = Digit of int
-and boolop =
+    | Digit of int
     | Equal
     | Not_Equal
-and boolval =
     | False
     | True
-and intop =
     | Plus
-and dollar_sign = | Dollar_Sign
-and open_brace = | Open_Brace
-and close_brace = | Close_Brace
-and open_paren = | Open_Paren
-and close_paren = | Close_Paren
-and equals = | Equals
-and _while = | While
-and _if = | If
-and quote = Quote
+    | Dollar_Sign
+    | Open_Brace
+    | Close_Brace
+    | Open_Paren
+    | Close_Paren
+    | Equals
+    | While
+    | If
+    | Quote
 ;;
 
 (* oop LL(1) queue *)
