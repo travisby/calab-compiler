c99bf6cf563299192e6f1372b6635785b8f815bf
Travis Beatty <travisby@gmail.com>
2014-03-02 12:58:22 -0500

    Removed Lex prefix in parse

--- parse.ml
+++ parse.ml
@@ -101,7 +101,7 @@ and parse_block tokens =
     (* this is the return value *)
     Block (ob, statement_list, cb)
 and parse_open_brace tokens = match tokens#pop with
-    | Lex.T_Open_Brace _ -> Open_Brace
+    | T_Open_Brace _ -> Open_Brace
     | x -> raise (Expected_Something_Else("Open Brace", x))
 and parse_close_brace tokens = match tokens#pop with
     | T_Close_Brace _ -> Close_Brace
@@ -112,11 +112,11 @@ and parse_close_brace tokens = match tokens#pop with
  * know whether to issue an empty statement list, or an unexpected token error
  *)
 and parse_statement_list tokens = match tokens#peek with
-    | Lex.T_Print x ->
+    | T_Print x ->
             let print_statement = parse_print_statement tokens in
             let statement_list = parse_statement_list tokens in
             Statement_List (Statement_Print_Statement (print_statement), statement_list)
-    | Lex.T_Id x ->
+    | T_Id x ->
             let assignment_statement = parse_assignment_statement tokens in
             let statement_list = parse_statement_list tokens in
             Statement_List (Statement_Assignment_Statement (assignment_statement), statement_list)
@@ -125,21 +125,21 @@ and parse_statement_list tokens = match tokens#peek with
     | T_String x -> Statement_List (Statement_Var_Decl (parse_var_decl_statement tokens), parse_statement_list tokens)
     | T_Boolean x -> Statement_List (Statement_Var_Decl (parse_var_decl_statement tokens), parse_statement_list tokens)
     *)
-    | Lex.T_While x ->
+    | T_While x ->
             let while_statement = parse_while_statement tokens in
             let statement_list = parse_statement_list tokens in
             Statement_List (Statement_While_Statement (while_statement), statement_list)
-    | Lex.T_If x ->
+    | T_If x ->
             let if_statement = parse_if_statement tokens in
             let statement_list = parse_statement_list tokens in
             Statement_List (Statement_If_Statement (if_statement), statement_list)
-    | Lex.T_Open_Brace x ->
+    | T_Open_Brace x ->
             let block = parse_block tokens in
             let statement_list = parse_statement_list tokens in
             Statement_List (Statement_Block (block), statement_list)
     | _ -> Emtpy_Statement_List
 and parse_print_statement tokens = match tokens#pop with
-    | Lex.T_Print _ ->
+    | T_Print _ ->
             let op = parse_open_paren tokens in
             let expr = parse_expr tokens in
             let cp = parse_close_paren tokens in
@@ -158,38 +158,38 @@ and parse_var_decl_statement tokens =
     let _id = parse_id tokens in
     Var_Decl (the_type, _id)
 and parse_while_statement tokens = match tokens#pop with
-    | Lex.T_While _ ->
+    | T_While _ ->
             let boolean_expr = parse_boolean_expr tokens in
             let block = parse_block tokens in
             While_Statement (boolean_expr, block)
     | x -> raise (Expected_Something_Else("While", x))
 and parse_if_statement tokens = match tokens#pop with
-    | Lex.T_If _ ->
+    | T_If _ ->
             let boolean_expr = parse_boolean_expr tokens in
             let block = parse_block tokens in
             If_Statement (boolean_expr, block)
     | x -> raise (Expected_Something_Else("If", x))
 and parse_open_paren tokens = match tokens#pop with
-    | Lex.T_Open_Paren _ -> Open_Paren
+    | T_Open_Paren _ -> Open_Paren
     | x -> raise (Expected_Something_Else("Open Paren", x))
 and parse_expr tokens = match tokens#peek with
-    | Lex.T_Digit _ -> Expr_Int_Expr (parse_int_expr tokens)
+    | T_Digit _ -> Expr_Int_Expr (parse_int_expr tokens)
     | T_Double_Quote _ -> Expr_String_Expr (parse_string_expr tokens)
-    | Lex.T_Id _ -> Expr_Id_Expr (parse_id tokens)
+    | T_Id _ -> Expr_Id_Expr (parse_id tokens)
     | x -> raise (Expected_Something_Else("Digit | Quote | Id", x))
 and parse_close_paren tokens = match tokens#pop with
-    | Lex.T_Close_Paren _ -> Close_Paren
+    | T_Close_Paren _ -> Close_Paren
     | x -> raise (Expected_Something_Else("Close Paren", x))
 and parse_id tokens = match tokens#peek with 
-    | Lex.T_Id x -> Id (parse_char tokens)
+    | T_Id x -> Id (parse_char tokens)
     | x -> raise (Expected_Something_Else("Id", x))
 and parse_equals tokens = match tokens#pop with
-    | Lex.T_Assignment _ -> Equals
+    | T_Assignment _ -> Equals
     | x -> raise (Expected_Something_Else("=", x))
 and parse_type tokens = match tokens#pop with
-    | Lex.T_Int _ -> Int
-    | Lex.T_Boolean _ -> Boolean
-    | Lex.T_String _ -> String
+    | T_Int _ -> Int
+    | T_Boolean _ -> Boolean
+    | T_String _ -> String
     | x -> raise (Expected_Something_Else("int | boolean | string", x))
 and parse_boolean_expr tokens =
     let op = parse_open_paren tokens in
@@ -209,24 +209,24 @@ and parse_string_expr tokens =
     let quote2 = parse_quote tokens in
     String_Expr (quote1, char_list, quote2)
 and parse_char tokens = match tokens#pop with 
-    | Lex.T_Char x -> Char (String.get x.value 0)
+    | T_Char x -> Char (String.get x.value 0)
     | x -> raise (Expected_Something_Else("a character", x))
 and parse_boolop tokens = match (tokens#pop) with
-    | Lex.T_Equality _ -> Equal
-    | Lex.T_Inequality _ -> Not_Equal
+    | T_Equality _ -> Equal
+    | T_Inequality _ -> Not_Equal
     | x -> raise (Expected_Something_Else("== | !=", x))
 and parse_digit tokens = match (tokens#pop) with
-    | Lex.T_Digit x -> Digit (int_of_string x.value)
+    | T_Digit x -> Digit (int_of_string x.value)
     | x -> raise (Expected_Something_Else("a number", x))
 and parse_intop tokens = match (tokens#pop) with
-    | Lex.T_Plus _ -> Plus
+    | T_Plus _ -> Plus
     | x -> raise (Expected_Something_Else("+", x))
 and parse_quote tokens = match (tokens#pop) with
-    | Lex.T_Double_Quote _ -> Quote
+    | T_Double_Quote _ -> Quote
     | x -> raise (Expected_Something_Else("\"", x))
 and parse_char_list tokens = match (tokens#pop) with
-    | Lex.T_String x -> Char_List (x.value)
+    | T_String x -> Char_List (x.value)
     | x -> raise (Expected_Something_Else("a string", x))
 and parse_dollar_sign tokens = match (tokens#pop) with
-    | Lex.T_Dollar_Sign _ -> Dollar_Sign
+    | T_Dollar_Sign _ -> Dollar_Sign
     | x -> raise (Expected_Something_Else("dollar sign", x))
