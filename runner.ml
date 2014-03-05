556987bd8089ab5d885c757561af050299522f90
Travis Beatty <travisby@gmail.com>
2014-03-05 18:53:31 -0500

    used the param from cli for filename

--- runner.ml
+++ runner.ml
@@ -1,5 +1,5 @@
-let file = "" in
-let file_string = Utils.file_name_to_string file in
+let file_name = Array.get Sys.argv 1 in
+let file_string = Utils.file_name_to_string file_name in
 Bolt.Logger.log "logger" Bolt.Level.TRACE "started" ~file: "runner";
 try Parse.parse (Lex.lex file_string)
 with _ -> exit 1
