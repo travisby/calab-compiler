c55659c48a7dd4444374486071990d30080687b0
Travis Beatty <travisby@gmail.com>
2014-03-06 08:16:56 -0500

    Fixed our last-exception error reporting

--- compiler.ml
+++ compiler.ml
@@ -1,7 +1,9 @@
 let file_name = Array.get Sys.argv 1 in
 let file_string = Utils.file_name_to_string file_name in
-Bolt.Logger.log "logger" Bolt.Level.TRACE "started" ~file: "runner";
+Bolt.Logger.log "logger" Bolt.Level.TRACE "started" ~file: "compiler";
 try Parse.parse (Lex.lex file_string)
-with _ ->
+with x ->
     print_string "Cannot recover.  Exitting";
-    exit 1
+    print_newline ();
+    raise x
+Bolt.Logger.log "logger" Bolt.Level.TRACE "finished" ~file: "compiler";
