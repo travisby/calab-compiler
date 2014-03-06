9c4e3f12c4e437063228c24d2bedceec5e6877a0
Travis Beatty <travisby@gmail.com>
2014-03-05 19:26:26 -0500

    Added catchall error message

--- compiler.ml
+++ compiler.ml
@@ -2,4 +2,6 @@ let file_name = Array.get Sys.argv 1 in
 let file_string = Utils.file_name_to_string file_name in
 Bolt.Logger.log "logger" Bolt.Level.TRACE "started" ~file: "runner";
 try Parse.parse (Lex.lex file_string)
-with _ -> exit 1
+with _ ->
+    print_string "Cannot recover.  Exitting";
+    exit 1
