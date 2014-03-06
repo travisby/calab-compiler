09c79e39108f289ab2cb73bbb8934e12ac31d3af
Travis Beatty <travisby@gmail.com>
2014-03-06 11:10:22 -0500

    Added an extra space in our errors

--- compiler.ml
+++ compiler.ml
@@ -3,7 +3,9 @@ let file_string = Utils.file_name_to_string file_name in
 Bolt.Logger.log "logger" Bolt.Level.TRACE "started" ~file: "compiler";
 try Parse.parse (Lex.lex file_string)
 with x ->
+    print_newline ();
     print_string "Cannot recover.  Exitting";
     print_newline ();
     raise x
+;;
 Bolt.Logger.log "logger" Bolt.Level.TRACE "finished" ~file: "compiler";
