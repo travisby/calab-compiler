5a8ce3144ca997913d808dc1177be22c16928100
Travis Beatty <travisby@gmail.com>
2014-03-06 11:53:48 -0500

    Added stupidly simple test runner

---
+++ test.sh
@@ -0,0 +1,13 @@
+for expected in `ls examples/`
+do
+    echo EXPECTING TO $expected;
+    for file in `ls examples/$expected`
+    do
+        echo "test: " $file
+        result=`BOLT_FILE=./log_config ./compiler examples/$expected/$file`
+        warn=`echo $result | grep -i warn | wc -l`
+        error=`echo $result | grep -i error | wc -l`
+        echo "Warn Count: " $warn
+        echo "Error Count: " $error
+    done
+done
