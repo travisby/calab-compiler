97637006ebe1dd35f790fc56f7b4836ef0d8691c
Travis Beatty <travisby@gmail.com>
2014-03-17 10:46:01 -0400

    Added spaces around !=

--- lex.ml
+++ lex.ml
@@ -156,7 +156,7 @@ let lex str =
 
     (* ensure there is no _ already in the program *)
     if String.contains str '_' then raise CannotContainUnderscoreError else ();
-    let my_reg = Str.regexp "==\\|=\\|[(){}$\"\\|\\+]" in
+    let my_reg = Str.regexp "==\\|!=\\|=\\|[(){}$\"\\|\\+]" in
     let add_spaces_func str = (Str.global_replace my_reg " \\0 " str) in
     let remove_blanks_func = List.filter (fun x -> not (Str.string_match (Str.regexp "^$") x 0)) in
     let tokenize_by_line_func lineno queue = List.map (fun x -> tokenize queue lineno x) in
