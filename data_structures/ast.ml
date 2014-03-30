59b283243bab564bfe6697f0006f08a7bd9960e5
Travis Beatty <travisby@gmail.com>
2014-03-30 08:43:46 -0400

    Added AST data type to our project

---
+++ data_structures/ast.ml
@@ -0,0 +1,13 @@
+type ast =
+    | Program of ast
+    | Block of ast list
+    | Print_Statement of ast
+    | Assignment_Statement of ast * ast
+    | Var_Decl of ast * ast
+    | While_Statement of ast * ast
+    | If_Statement of ast * ast
+    | Int
+    | Boolean
+    | String
+    | Id of char
+;;
