883b1ab8d17a4e88bb723b052631be611edbe860
Travis Beatty <travisby@gmail.com>
2014-04-14 17:42:20 -0400

    Started testing for assignment 2

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
-for expected in `ls examples/`
+for expected in `ls assignment2_examples/`
 do
     echo '-------------------------------';
     echo EXPECTING TO $expected;
-    for file in `ls examples/$expected`
+    for file in `ls assignment2_examples/$expected`
     do
         echo "test: " $file
-        result=`BOLT_FILE=./log_config ./compiler examples/$expected/$file`
+        result=`BOLT_FILE=./log_config ./compiler assignment2_examples/$expected/$file`
         warn=`echo $result | grep -i warn | wc -l`
         error=`echo $result | grep -i error | wc -l`
         echo "Warn Count: " $warn
